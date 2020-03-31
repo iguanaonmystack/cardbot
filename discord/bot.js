2a3d418dfc061fde14d877526df802b5f77fd08f
Kathy Murdoch <kathy@nivan.net>
2020-03-31 20:59:21 +0100

    Implement (most of) ircbot.py in Node.js for Discord.
    Except the bit where it actually deals with the Game or Parser, of course.

--- discord/bot.js
+++ discord/bot.js
@@ -1,8 +1,7 @@
-// https://medium.com/davao-js/2019-tutorial-creating-your-first-simple-discord-bot-47fc836a170b
-
 // Run dotenv
 require('dotenv').config();
 
+// npm install --save discord.js dotenv
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -11,6 +10,27 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+    if (msg.author.bot) {
+        console.log(`Ignoring message from bot ${msg.author.username}`);
+        return;
+    }
+    var user = msg.author;
+    var channel = msg.channel;
+    var mlc = msg.content.toLowerCase();
+    var params = mlc.split(' ');
+
+    var origin = 'direct';
+    var reply_fn;
+    if (channel instanceof Discord.DMChannel) {
+        origin = 'direct';
+        reply_fn = function(text) { msg.reply(text); };
+    } else if (channel.name == process.env.GAME_CHANNEL) {
+        origin = 'channel';
+        reply_fn = function(text) { channel.send(text); };
+    }
+
+    reply_fn(`Will parse user ${user}, origin ${origin}, params ${params}`);
+
     if (msg.content === 'ping') {
         msg.reply('pong');
     }
